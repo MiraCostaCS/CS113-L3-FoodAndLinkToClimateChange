9823c45e5df0cb7d1e4f6ec21d39dc48f4832525
Nery Chapetón-Lamas <nerychapeton@gmail.com>
2026-05-14 17:21:48 -0700

    Implement SingleLinkedList class with basic methods
    This file defines a generic SingleLinkedList class with methods for adding, removing, and accessing elements, as well as checking for data presence and printing the list.

---
+++ SingleLinkedList.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class SingleLinkedList<T> {
+
+    // TODO: Static inner Node class
+
+
+    // TODO: First element in the list
+    
+    // TODO: Number of elements in the list
+    
+    // TODO: Default constructor
+
+
+    // TODO: Add element at the beginning of the list
+    public void addFirst(T data) {
+        // Students: implement this method
+    }
+
+    // TODO: Add element at the end of the list
+    public void addLast(T data) {
+        // Students: implement this method
+    }
+
+    // TODO: Remove the first element and return its data
+    public T removeFirst() {
+        // Students: implement this method
+        return null; // placeholder
+    }
+
+    // TODO: Check if the list contains the given data
+    public boolean contains(T data) {
+        // Students: implement this method
+        return false; // placeholder
+    }
+
+    // TODO: Get element by index (0-based)
+    public T get(int index) {
+        // Students: implement this method
+        return null; // placeholder
+    }
+
+    // TODO: Return the number of elements in the list
+    // public int size() {
+    //     return ??;
+    // }
+
+    // TODO: Print all elements in the list
+    public void printList() {
+        // Students: implement this method
+    }
+}
